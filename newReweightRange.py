e12aeeb0293015a585c2eaeefe7c0a0fe435256b
Steffen <steffen.albrecht@desy.de>
2020-02-14 11:21:18 +0100

    removed old stuff from newReweightRange.py

--- newReweightRange.py
+++ newReweightRange.py
@@ -89,38 +89,6 @@ if(__name__=="__main__"):
     # operators=["S0","S1","S2","M0","M1","M2","M3","M4","M5","M6","M7","T0","T1","T2","T5","T6","T7","T8","T9"]
     operators=["S0","S1","M0","M1","M2","M3","M4","M5","M6","M7","T0","T1","T2","T5","T6","T7","T8","T9"]
     sets=getJsonFromCSV(newModel=False,csvFileName='range.csv')
-    # sets=json.load(open('range.json','r'))
-    # with open(BosonChannel+'Range.csv','rb') as csvfile:
-    #     snippet=''
-    #     snippet2=''
-    #     NPoint=0
-    #     setreader=csv.DictReader(csvfile)
-    #     for row in setreader:
-    #         sets.update({row['parameter']:[
-    #                 int(row['anoinput']),
-    #                 int(row['Npoints']),
-    #                 float(row['start']),
-    #                 float(row['stepsize'])
-    #                 ]})
-
-    #         snippet+='//%s=[%i,%.2f,%.2f,%.2f]\n'%(row['parameter'],int(row['Npoints']),float(row['stepsize']),float(row['start']),float(row['end']))
-    #         snippet+='for(unsigned int i=0; i<%i; i++){\n'%int(row['Npoints'])
-    #         snippet+='reweight_names.push_back(getParName("%s",%.2ff,%.2ff,i));\n'%(row['parameter'],float(row['start']),float(row['stepsize']))
-    #         snippet+='}\n'
-
-    #         NPoint+=int(row['Npoints'])
-    #         snippet2+='if(parameter_index<%i){\n'%NPoint
-    #         snippet2+='std::string parameter_string="%s";\n'%row['parameter']
-    #         snippet2+='reweight_names.push_back(getParName("%s",%.2ff,%.2ff,parameter_index-%i));\n'%(row['parameter'],float(row['start']),float(row['stepsize']),(NPoint-int(row['Npoints'])))
-    #         snippet2+='}else '
-
-    #     print('snippet for MjjHists:')
-    #     print(snippet)
-
-    #     print('snippet for PDFHists:')
-    #     print(snippet2)
-
-
     
     print(sets)
     with open(BosonChannel+"Range.dat","wt") as fout:
